0caa01c9999e716dfa2ac97bf7cb9004bac5da7b
unknown <newgikwad@gamil.com>
2021-10-08 09:33:24 +0530

    first commit

---
+++ src/test/java/com/newit/GithubProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
